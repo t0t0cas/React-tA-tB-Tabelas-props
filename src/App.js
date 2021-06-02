e8225a314b9ec5555d8e1f9bfc0584ddbf3b9a52
António Fialho <aluno21895@ipt.pt>
2021-06-02 10:37:39 +0100

    Separação de um componente em dois sub-componentes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@
 //importar da pasta 'node_modules' a package do React 
 import React from 'react'
 import Tabela from './Tabela'
+import Tabela2 from './Tabela2'
 
 class App extends React.Component {
   //todas as classes terão, obrigatoriamente, um método chamado 'render'
@@ -14,7 +15,10 @@ class App extends React.Component {
       
       <h2>Tabela puramente 'hard coded'</h2>
       <Tabela />
-
+      <br />
+      {/* junção de dois componentes dentro  de um outro*/}
+      <h2>Tabela com componentes simples</h2>
+      <Tabela2 />
 
     </div>
     );
---
+++ src/Tabela2.js
@@ -0,0 +1,53 @@
+//Tabela.js
+//********************************
+
+import { render } from '@testing-library/react'
+import React from 'react'
+
+//função que devolve o Cabeçalho da Tabela
+function CabecalhoTabela() {
+    return (
+        <thead>
+            <tr>
+                <td>Nome</td>
+                <td>Apelido</td>
+            </tr>
+        </thead>
+    )
+}
+
+//definição da função que devolve o Corpo da tabela
+//faz exatamente o mesmo da linha 7
+const CorpoTabela = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>João</td>
+                <td>Lopes</td>
+            </tr>
+            <tr>
+                <td>Mario</td>
+                <td>Santos</td>
+            </tr>
+            <tr>
+                <td>Ana</td>
+                <td>Silva</td>
+            </tr>
+        </tbody>
+    )
+}
+
+//componente que junta os dois sub-componentes
+class Tabela2 extends React.Component {
+    render() {
+        return (
+            <table className="table table-hover">
+                <CabecalhoTabela />
+                <CorpoTabela />
+            </table>
+        )
+    }
+}
+
+export default Tabela2
+
